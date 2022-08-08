4a4805b07790eb16aa55e5b50fbf0e94894e5041
DilshodbekHoji <dilshod.usmanov.77@mail.ru>
2022-08-08 19:44:07 +0300

    class3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("It's my fisrt .....");
+// console.log("It's my fisrt .....");
+// var str = "Hello world!!!";
+// console.log(str.charAt(0));
+// var onclick = Date()
+function myFunction(){
+    document.getElementById("demo").innerHTML = "out html";
+}
+
+
